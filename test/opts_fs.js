95bd21ff82b51bbdf03a0222713b63e90ee60598
Rob Williamson <rob@robwilliamson.com>
2020-03-12 23:14:27 +0100

    Enable testing to run with later node versions

--- test/opts_fs.js
+++ test/opts_fs.js
@@ -1,4 +1,5 @@
 var mkdirp = require('../');
+var fs = require('fs');
 var path = require('path');
 var test = require('tap').test;
 var mockfs = require('mock-fs');
@@ -7,22 +8,23 @@ var _0755 = parseInt('0755', 8);
 
 test('opts.fs', function (t) {
     t.plan(5);
-    
+
     var x = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
     var y = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
     var z = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
-    
-    var file = '/beep/boop/' + [x,y,z].join('/');
-    var xfs = mockfs.fs();
-    
-    mkdirp(file, { fs: xfs, mode: _0755 }, function (err) {
+
+    var file = '/beep/boop/' + [x, y, z].join('/');
+    mockfs();
+
+    mkdirp(file, { fs, mode: _0755 }, function (err) {
         t.ifError(err);
-        xfs.exists(file, function (ex) {
+        fs.exists(file, function (ex) {
             t.ok(ex, 'created file');
-            xfs.stat(file, function (err, stat) {
+            fs.stat(file, function (err, stat) {
                 t.ifError(err);
                 t.equal(stat.mode & _0777, _0755);
                 t.ok(stat.isDirectory(), 'target not a directory');
+                mockfs.restore();
             });
         });
     });
--- test/opts_fs_sync.js
+++ test/opts_fs_sync.js
@@ -1,4 +1,5 @@
 var mkdirp = require('../');
+var fs = require('fs');
 var path = require('path');
 var test = require('tap').test;
 var mockfs = require('mock-fs');
@@ -7,21 +8,22 @@ var _0755 = parseInt('0755', 8);
 
 test('opts.fs sync', function (t) {
     t.plan(4);
-    
+
     var x = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
     var y = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
     var z = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
-    
-    var file = '/beep/boop/' + [x,y,z].join('/');
-    var xfs = mockfs.fs();
-    
-    mkdirp.sync(file, { fs: xfs, mode: _0755 });
-    xfs.exists(file, function (ex) {
+
+    var file = '/beep/boop/' + [x, y, z].join('/');
+    mockfs();
+
+    mkdirp.sync(file, { fs, mode: _0755 });
+    fs.exists(file, function (ex) {
         t.ok(ex, 'created file');
-        xfs.stat(file, function (err, stat) {
+        fs.stat(file, function (err, stat) {
             t.ifError(err);
             t.equal(stat.mode & _0777, _0755);
             t.ok(stat.isDirectory(), 'target not a directory');
+            mockfs.restore();
         });
     });
 });
